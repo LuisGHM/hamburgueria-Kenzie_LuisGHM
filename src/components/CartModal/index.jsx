292e1514170e17e1866b75b8669cea0dc15c3a9b
Luis Gustavo Hedel Marchiore <lgmarchioreh@gmail.com>
2023-08-06 16:43:46 -0300

    estilos globais e o módulo do header

--- src/components/CartModal/index.jsx
+++ src/components/CartModal/index.jsx
@@ -1,5 +1,6 @@
 import { MdClose } from "react-icons/md";
 import { CartItemCard } from "./CartItemCard";
+import styles from "./style.module.scss";
 
 export const CartModal = ({ cartList, isOpen, setIsOpen }) => {
    const total = cartList.reduce((prevValue, product) => {
@@ -8,26 +9,28 @@ export const CartModal = ({ cartList, isOpen, setIsOpen }) => {
 
    return (
       isOpen ? (
-         <div role="dialog">
-            <div>
-               <h2>Carrinho de compras</h2>
-               <button aria-label="close" title="Fechar">
-                  <MdClose size={21} />
-               </button>
-            </div>
-            <div>
-               <ul>
-                  {cartList.map((product) => (
-                     <CartItemCard key={product.id} product={product} />
-                  ))}
-               </ul>
-            </div>
-            <div>
+         <div role="dialog" className={styles.modalOverlay}>
+            <div className={styles.modalBox}>
                <div>
-                  <span>Total</span>
-                  <span>{total.toLocaleString('pt-BR', { style: "currency", currency: "BRL"})}</span>
+                  <h2>Carrinho de compras</h2>
+                  <button aria-label="close" title="Fechar" onClick={() => setIsOpen(false)}>
+                     <MdClose size={21} />
+                  </button>
                </div>
-               <button>Remover todos</button>
+               <div>
+                  <ul>
+                     {cartList.map((product) => (
+                        <CartItemCard key={product.id} product={product} />
+                     ))}
+                  </ul>
+               </div>
+               <div>
+                  <div>
+                     <span>Total</span>
+                     <span>{total.toLocaleString('pt-BR', { style: "currency", currency: "BRL"})}</span>
+                  </div>
+                  <button>Remover todos</button>
+               </div>
             </div>
          </div>
       ) : null 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Logo from "../../assets/Logo.svg";
 import { MdSearch, MdShoppingCart } from "react-icons/md";
+import styles from "./style.module.scss";
 
 export const Header = ({ setSearchValue, cartList, setIsOpen }) => {
    const [value, setValue] = useState("");
@@ -10,19 +11,23 @@ export const Header = ({ setSearchValue, cartList, setIsOpen }) => {
    }, [value, setSearchValue]);
 
    return (
-      <header>
-         <img src={Logo} alt="Logo Kenzie Burguer" />
-         <div>
-            <button onClick={() => setIsOpen(true)}>
-               <MdShoppingCart size={21} />
-               <span>{cartList.length}</span>
-            </button>
-            <form>
-               <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
-               <button type="submit">
-                  <MdSearch size={21} />
-               </button>
-            </form>
+      <header className={styles.header}>
+         <div className="container">
+            <div className={styles.headerContainer}>
+               <div className={styles.optContainer}>
+                  <img src={Logo} alt="Logo Kenzie Burguer" />
+                  <button onClick={() => setIsOpen(true)}>
+                     <MdShoppingCart size={21} />
+                     <span>{cartList.length}</span>
+                  </button>
+               </div>
+               <form className={styles.formInput}>
+                  <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+                  <button type="submit">
+                     <MdSearch size={21} />
+                  </button>
+               </form>
+            </div>
          </div>
       </header>
    );
